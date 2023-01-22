8a0d713c1dd3f4cdb8d27d68b1fdb75d1e936c73
miguelangellerma <miguellangel770@gmail.com>
2023-01-22 18:21:20 -0500

    added gameOver selector, added game over fx in level container

--- src/pages/game/candy-tiles/level-container/LevelContainer.tsx
+++ src/pages/game/candy-tiles/level-container/LevelContainer.tsx
@@ -1,22 +1,28 @@
-import { useRef } from 'react';
 import TileGrid from './grids/TileGrid';
 import ItemGrid from './grids/ItemGrid';
 import LevelManager from './level-manager/LevelManager';
 import DelayComponent from '../../../../components/DelayComponent';
 import { ANIMATION_TIME_MS } from '../../../../config';
 import FXGrid from './grids/FXGrid';
+import { useRecoilValue } from 'recoil';
+import { gameOverState } from '../selectors/gameOver';
+import anime from 'animejs';
 
+const animateLevelContainerFilter = () => {
+	anime({
+		targets: '#level-container',
+		filter: ['grayscale(0)', 'grayscale(0.8)'],
+		duration: 1000,
+		easing: 'linear'
+	});
+};
 const LevelContainer = () => {
-	const levelGridElement = useRef<HTMLElement | null>(null);
+	const gameOver = useRecoilValue(gameOverState);
+
+	gameOver && animateLevelContainerFilter();
 
 	return (
-		<section
-			className='grow aspect-square rounded-lg overflow-hidden relative select-none'
-			ref={levelGridElement}
-			/* style={{
-				filter: levelMoves.spendAllMoves ? 'grayscale(0.8)' : '',
-			}} */
-		>
+		<section id="level-container" className="grow aspect-square rounded-lg overflow-hidden relative select-none">
 			<LevelManager></LevelManager>
 			<TileGrid></TileGrid>
 			<DelayComponent delayMs={ANIMATION_TIME_MS}>
---
+++ src/pages/game/candy-tiles/selectors/gameOver.ts
@@ -0,0 +1,15 @@
+import { selector } from 'recoil';
+import { finishedMovingState } from '../atoms/finishedMoving';
+import { levelMovesState } from '../atoms/levelMoves';
+import { possibleCombinationsState } from '../atoms/possibleCombinations';
+
+export const gameOverState = selector<boolean>({
+	key: 'gameOver',
+	get: ({ get }) => {
+		const finishedMoving = get(finishedMovingState);
+		const levelMoves = get(levelMovesState);
+		const possibleCombinations = get(possibleCombinationsState);
+
+		return finishedMoving && (levelMoves.spendAllMoves || !possibleCombinations);
+	},
+});
