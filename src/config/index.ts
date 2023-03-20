f418fed58595b7acffacb5170ac80e0c0a05cb12
miguelangellerma <miguellangel770@gmail.com>
2023-03-20 15:32:28 -0500

    updated config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-export const COLUMN_NUMBER = parseInt(import.meta.env.VITE_COLUMN_NUMBER);
-export const ROW_NUMBER = parseInt(import.meta.env.VITE_ROW_NUMBER);
+export const COLUMN_NUMBER = 9;
+export const ROW_NUMBER = 9;
 export const GRID_NUMBER = COLUMN_NUMBER * ROW_NUMBER;
 export const TILE_COUNT = COLUMN_NUMBER * ROW_NUMBER;
-export const ANIMATION_TIME_MS = parseInt(import.meta.env.VITE_ANIMATION_TIME_MS);
+export const ANIMATION_TIME_MS = 300;
 export const ANIMATION_TIME_SECONS = ANIMATION_TIME_MS / 1000;
 export const SCORE_RATING = {
 	oneStar: 70,
